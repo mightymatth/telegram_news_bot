a32a017b30e981240fa438479cb1bef82c90d0a7
Matija Pevec <matija.pevec@ingemark.com>
2017-09-26 19:53:55 +0200

    Added TheHackerNews

--- app/utils/telegram_helpers.rb
+++ app/utils/telegram_helpers.rb
@@ -12,28 +12,7 @@ module TelegramHelpers
     uri.to_s
   end
 
-  # def send_link_preview_message(bot, chat_id, link)
-  #   html_link = "[»](#{link})"
-  #   bot.api.send_message(chat_id: chat_id, text: html_link,
-  #                        parse_mode: 'Markdown',
-  #                        reply_markup: {hide_keyboard: true})
-  # end
-
   def log_sending(subject, message_from)
     puts "[#{Time.now}] Sending '#{subject}' to #{message_from.first_name} #{message_from.last_name} (#{message_from.username})"
   end
-
-
-
-  # def set_subtitle(site_sku, category_sku, index)
-  #   "#{section_label.to_s.titlecase} (#{index + 1}/#{Section.get(category_sku).size})"
-  # end
-
-  def home_keyboard_markup
-    Telegram::Bot::Types::ReplyKeyboardMarkup.new(
-      keyboard: ['Most Popular', 'Most Recent', 'Croatia', 'World'],
-      one_time_keyboard: true)
-  end
-
-
 end
