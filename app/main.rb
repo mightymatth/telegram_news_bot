d422e23bc85f608c6e272daa7e834b3640811ad4
Matija Pevec <matija.pevec@ingemark.com>
2019-02-27 15:33:06 +0100

    Minor cleaning

--- app/main.rb
+++ app/main.rb
@@ -1,11 +1,8 @@
-require 'telegram/bot'
 require 'dotenv/load'
-require_relative 'utils/telegram_helpers'
+require 'telegram/bot'
 require_relative 'utils/query_resolver'
 
 class TgNewsBot
-  extend TelegramHelpers
-
   Telegram::Bot::Client.run(ENV['TELEGRAM_BOT_TOKEN']) do |bot|
     begin
       bot.listen do |message|
